2c791b44c12350603da6da55a8961b7953d7a31d
marcossalazars <102389383+marcossalazars@users.noreply.github.com>
2023-09-29 00:25:38 -0600

    Render test

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
+@app.route('/test', methods=['GET'])
 def api():
-    data = request.get_json()
+    data = {
+        data: 'test'
+    }
     return jsonify(data)
 
 if __name__ == '__main__':
